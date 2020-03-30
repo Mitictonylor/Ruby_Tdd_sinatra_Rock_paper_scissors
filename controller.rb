f7502468f65ad498a6e975e7b53908d5af137005
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-30 19:17:22 +0100

    add other get"

--- controller.rb
+++ controller.rb
@@ -5,8 +5,25 @@ require_relative('./models/logic.rb')
 also_reload('./models/*')
 
 get '/rock_paper_scissors/:player1/:player2' do
-  player1 = params['player1'].to_s
-  player2 = params['player2'].to_s
+
+  player1 = params['player1'].chomp
+  player2 = params['player2'].chomp
   @result = Logic.rock_paper_scissors(player1,player2)
  erb(:result)
+end
+
+get '/welcome' do
+  erb(:welcome)
+end
+
+get '/rock_paper_scissors' do
+  erb(:add_two_players)
+end
+
+get '/rock_paper_scissors/:player1' do
+  erb(:add_one_player)
+end
+
+get '/' do
+erb(:add_game)
 end
